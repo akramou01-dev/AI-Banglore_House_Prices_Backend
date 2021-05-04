c46f6f08871c9bd1e241595f66464f43b81bac07
Akram Ouardas <a.ouardas@esi-sba.dz>
2021-05-04 06:45:01 +0100

    adding the Ui en deploying the projet to AWS

--- server/server.py
+++ server/server.py
@@ -1,38 +1,37 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-
+import utils
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route('/', methods=['GET'])
 def test():
-    return "<h1>hey from the home page</h1>"
+    return "<h3>hey from the home page</h3>"
 
-app.route("/locations",methods=["GET"])
-def get_locations(): 
+
+@app.route("/locations", methods=["GET"])
+def get_locations():
     response = jsonify({
-        "location":utils.get_locations()
+        "locations": utils.get_locations()
     })
-    response.headers.add("Access-Control-Allow-Origin","*")
+    response.headers.add("Access-Control-Allow-Origin", "*")
     return response
 
 
-
-app.route("/predict_price",methods=["POST"])
-def predict_home_price(): 
+@app.route("/predict_price", methods=["POST"])
+def predict_home_price():
     total_sqft = float(request.form["total_sqft"])
     bhk = int(request.form["bhk"])
     bath = int(request.form["bath"])
     location = request.form["location"]
 
     response = jsonify({
-        "estimated_price" : utils.get_estimated_price(lcoation,total_sqft,bath,bhk)
+        "estimated_price": utils.get_estimated_price(location, total_sqft, bath, bhk)
     })
     return response
-    
 
-if __name__ =="__main__": 
+
+if __name__ == "__main__":
     print("The server is starting...")
+    utils.load_saved_artifacts()
     app.run()
-
-
--- server/utils.py
+++ server/utils.py
@@ -7,24 +7,27 @@ __data_columns = None
 __model = None
 
 
-def get_estimated_price(location,sqft,bath,bhk):
-    try: 
+def get_estimated_price(location, sqft, bath, bhk):
+    try:
         loca_index = __data_columns.index(location.lower())
     except:
         loca_index = -1
 
-    x=np.zeros(len(__data_columns))
+    x = np.zeros(len(__data_columns))
     x[0] = sqft
     x[1] = bath
     x[2] = bhk
-    if loca_index >=0: 
-        x[loca_index] =1
+    if loca_index >= 0:
+        x[loca_index] = 1
 
-    return round(__model.predict([x])[0],2)
+    return round(__model.predict([x])[0], 2)
 
 
 def get_locations():
-    return __locations
+    # with open("./artifacts/columns.json", "r")as f:
+    #     __data_columns = json.load(f)["data_columns"]
+    #     locations = __data_columns[3:]
+        return __locations
 
 
 def load_saved_artifacts():
@@ -37,13 +40,11 @@ def load_saved_artifacts():
         __data_columns = json.load(f)["data_columns"]
         __locations = __data_columns[3:]
 
+
+
     with open("./artifacts/Banglore_home_prices_model.pickle", "rb") as f:
         __model = pickle.load(f)
 
 
 if __name__ == "__main__":
-    load_saved_locations()
-    # print(get_estimated_price("neeladri nagar",250,3,3))
-    # print(get_estimated_price("neeladri nagar",250,3,3))
-    # print(get_estimated_price("1st phase jp nagar",1000,2,2))
-    # print(get_estimated_price("9th phase jp nagar",500,1,2))
+    load_saved_artifacts()
